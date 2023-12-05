5c7f3fde9c32cb28474bec9ac9d7d6253209ef90
Gabriela Vasquez Gutierrez <gabriela.vsquezg@gmail.com>
2023-12-04 21:43:56 -0500

    Booleanas explosión y está muerta

--- classes/Particula.js
+++ classes/Particula.js
@@ -1,17 +1,21 @@
 class Particula {
   constructor() {
-    this.tamInicial = random(50, 100);
+    this.tamInicial = random(25, 50);
     this.tam = this.tamInicial;
     this.rad = this.tam / 2;
     this.pos = createVector(windowWidth / 2, windowHeight + this.rad);
 
-    this.vel = createVector(0, random(5, 10));
+    this.vel = createVector(0, random(5, 11));
     this.vel.rotate(random(150, 210));
-    this.acel = createVector(random(-0.1, 0.1), 0.1);
-    this.tiempoDeVidaInicial = random(100, 200);
+    this.acel = createVector(random(-0.01, 0.01), 0.1);
+
+    this.tiempoDeVidaInicial = Math.ceil(random(50, 150));
     this.tiempoDeVida = this.tiempoDeVidaInicial;
+    this.estaMuerta = false;
+
+    this.explotar = false;
   }
-  // Metodo -> method update
+
   update() {
     this.vel.add(this.acel);
     this.pos.add(this.vel);
@@ -26,16 +30,20 @@ class Particula {
       0
     );
 
-    // if (this.tiempoDeVida <= 0) {
-    //   this.estaMuerta = true;
-    //  circle(this.pos.x, this.pos.y, this.tamInicial);
-    //}
-    //print("ups, me mori!");
+    if (this.tiempoDeVida <= 0) {
+      this.estaMuerta = true;
+      circle(this.pos.x, this.pos.y, this.tamInicial);
+    }
+
+    if (this.explotar >= 35) {
+      this.explotar = true;
+      circle(this.pos.x, this.pos.y, this.tamInicial);
+    }
   }
-  // Metodo -> method display
+
   display() {
     fill(255);
-    //noStroke();
+    noStroke();
     circle(this.pos.x, this.pos.y, this.tam);
   }
 }
--- sketch.js
+++ sketch.js
@@ -1,17 +1,23 @@
-let ps = [];
+let sis = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   angleMode(DEGREES);
   p = new Particula();
-  ps.push(p);
+  sis.push(p);
 }
 
 function draw() {
-  background(0, 70);
-  for (let i = 0; i < ps.length; i++) {
-    ps[i].update();
-    ps[i].display();
+  background(0, 25);
+  for (let i = 0; i < sis.length; i++) {
+    if (sis[i].estaMuerta) {
+      sis.splice(i, 1);
+    }
+  }
+
+  for (let i = 0; i < sis.length; i++) {
+    sis[i].update();
+    sis[i].display();
     print("funciona");
   }
   //for (let i = 0; i<ps.length; i++);
